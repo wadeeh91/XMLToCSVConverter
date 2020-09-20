7f6008997b1245d3572eafe71630449fc5be71da
Matti Räsänen <matti.rasanen91@gmail.com>
2020-09-20 18:57:21 +0300

    Changed reader to converter. Added also absolutepath to resources folder

--- code/converter.py
+++ code/converter.py
@@ -14,8 +14,6 @@ class foodData():
 def readXml(file):
     
     doc = xml.dom.minidom.parse(file)
-    # print(doc.nodeName)
-    # print(doc.firstChild.tagName)
 
     foods = doc.getElementsByTagName("food")
     for node in foods:
@@ -28,24 +26,19 @@ def readXml(file):
         for name in names:
             nameText = name.childNodes[0].nodeValue
             f.name = nameText
-            # print(nameText)
 
         for price in prices:
             nameText = price.childNodes[0].nodeValue
             f.price = nameText
-            # print(nameText) 
 
         for desc in descs:
             nameText = desc.childNodes[0].nodeValue
             f.description = nameText
-            # print(nameText) 
 
         for cal in cals:
             nameText = cal.childNodes[0].nodeValue
             f.calories = nameText
-            # print(nameText)
-        
-        # print("-----------------")
+  
         foodDataList.append(f)
 
 def prepareFile(file):
@@ -58,7 +51,6 @@ def prepareFile(file):
 
 def writeToCsv(file):
     separator = ";"
-    # f = open(file, "w+")
     f = prepareFile(file)
     for fData in foodDataList:
         line = fData.name+separator+fData.description+separator+fData.price+separator+fData.calories+"\n"
@@ -68,12 +60,10 @@ def writeToCsv(file):
 
 def main():
     print("Reading xml")
-    readXml("breakfast_menu.xml")
+    readXml("F:/Dev/2020/Python/Resources/breakfast_menu.xml")
     print("Writing to csv")
-    writeToCsv("breakfast_menu.csv")
+    writeToCsv("F:/Dev/2020/Python/Resources/breakfast_menu.csv")
     print("All ready! :)")
-    # print("Item exists: "+str(path.exists("breakfast_menu.csv")))
-    # print(prepareFile("breakfast_menu.csv"))
 
 if __name__ == "__main__":
     main()
